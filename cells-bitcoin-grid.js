1d2f43cf872e7f16da5c8205cc5b51b02eacf6de
rportocarrero <rportocarrero@gmail.com>
2019-11-10 18:12:32 -0500

    Github

---
+++ cells-bitcoin-grid.js
@@ -0,0 +1,66 @@
+{
+  const {
+    html,
+  } = Polymer;
+  /**
+    `<cells-bitcoin-grid>` Description.
+
+    Example:
+
+    ```html
+    <cells-bitcoin-grid></cells-bitcoin-grid>
+    ```
+
+    ## Styling
+    The following custom properties and mixins are available for styling:
+
+    ### Custom Properties
+    | Custom Property     | Selector | CSS Property | Value       |
+    | ------------------- | -------- | ------------ | ----------- |
+    | --cells-fontDefault | :host    | font-family  |  sans-serif |
+    ### @apply
+    | Mixins    | Selector | Value |
+    | --------- | -------- | ----- |
+    | --cells-bitcoin-grid | :host    | {} |
+
+    * @customElement
+    * @polymer
+    * @extends {Polymer.Element}
+    * @demo demo/index.html
+  */
+  class CellsBitcoinGrid extends Polymer.Element {
+
+    static get is() {
+      return 'cells-bitcoin-grid';
+    }
+
+    static get properties() {
+      return {
+        headers: { type: Array, value: [{
+          label: ''
+        }, {
+          label: 'price',
+          id: 'price',
+          sortable: true
+        }, {
+          label: 'description',
+          id: 'description',
+          sortable: false
+        }]},
+
+        rows: { type: Array, value: [] }
+      };
+    }
+
+    static get template() {
+
+      return html `
+          <cells-sortable-table class="simple" show-sort-buttons sort-by="price" rows="[[rows]]" column-headers="[[headers]]">
+          </cells-sortable-table>
+      
+      `;
+    }
+  }
+
+  customElements.define(CellsBitcoinGrid.is, CellsBitcoinGrid);
+}
